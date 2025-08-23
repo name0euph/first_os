4baa9362bbf2c8eece83f23e71876ef880477003
name0euph <name0euph@gmail.com>
2025-08-24 00:27:50 +0900

    feat: フォント作成

--- kernel/font.cpp
+++ kernel/font.cpp
@@ -6,33 +6,29 @@
 
 #include "font.hpp"
 
+// リンカが埋め込んだフォントバイナリのシンボルを参照
+extern const uint8_t _binary_hankaku_bin_start; // 先頭アドレス
+extern const uint8_t _binary_hankaku_bin_end; // 末尾の次
+extern const uint8_t _binary_hankaku_bin_size; // 全体サイズ
 
-const uint8_t kFontA[16] = {
-  0b00000000, //
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b01111110, //  ******
-  0b01000010, //  *    *
-  0b01000010, //  *    *
-  0b01000010, //  *    *
-  0b11100111, // ***  ***
-  0b00000000, //
-  0b00000000, //
-};
+// 文字cに対応するフォントデータ先頭へのポインタを返す関数
+const uint8_t* GetFont(char c) {
+  auto index = 16 * static_cast<unsigned int>(c); // 文字cのコード値×16バイト
+  if (index >= reinterpret_cast<uintptr_t>(&_binary_hankaku_bin_size)) {
+    return nullptr;
+  }
+  // 先頭アドレスにindexバイトを足して、その文字の先頭ポインタを返す
+  return &_binary_hankaku_bin_start + index;
+}
 
 void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
-  if (c != 'A') {
+  const uint8_t* font = GetFont(c);
+  if (font == nullptr) {
     return;
   }
   for (int dy = 0; dy < 16; ++dy) {
     for (int dx = 0; dx < 8; ++dx) {
-      if ((kFontA[dy] << dx) & 0x80u) {
+      if ((font[dy] << dx) & 0x80u) {
         writer.Write(x + dx, y + dy, color);
       }
     }
